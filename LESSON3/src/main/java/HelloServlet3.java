871e116f514f6deb4fb95f1d775f28e24d8e71f3
marusawa.m <marusawa.m@gmail.com>
2023-10-06 14:55:47 +0900

    Code3_3/URLパターンの設定

---
+++ LESSON3/src/main/java/HelloServlet3.java
@@ -0,0 +1,25 @@
+// Code3_3
+// URLパターンの設定
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+// @WebServletアノテーションで設定（自由に変更可）
+// @WebServlet("/URLパターン")
+@WebServlet("/hello")
+public class HelloServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+	}
+
+}
