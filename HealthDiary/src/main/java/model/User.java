e69489db5a9d45de2ea2150276efc8c2b2df9954
marusawa.m <marusawa.m@gmail.com>
2023-10-17 09:55:05 +0900

    ユーザー情報のJavaBeans

--- HealthDiary/src/main/java/model/User.java
+++ HealthDiary/src/main/java/model/User.java
@@ -1,8 +1,30 @@
 package model;
 
-public class User {
+// ユーザー情報に関するJavaBeans
+
+import java.io.Serializable;
+
+public class User implements Serializable {
 	
-	private String name;
+	private String name;	// ログイン用の名前
+	private String pass;	// ログイン用のパスワード
 	
+	
+	// プロパティ
+	public User (String name, String pass) {
+		
+		this.name = name;
+		this.pass = pass;
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPass() {
+		return pass;
+	}
 
 }
