a903d5fcaefa724e21b5619d68c831c895cc0196
marusawa.m <marusawa.m@gmail.com>
2023-10-17 15:57:31 +0900

    投稿に関するコントローラ：修正

--- HealthDiary/src/main/java/servlet/Main.java
+++ HealthDiary/src/main/java/servlet/Main.java
@@ -15,7 +15,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.BmiCheckLogic;
+import model.Health;
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 
@@ -28,7 +31,7 @@ public class Main extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-		// つぶやきリストをアプリケーションスコープから取得
+		// 投稿リストをアプリケーションスコープから取得
 		// （ジェネリクスを利用してJavaBeansであるMutterを取得している）
 		ServletContext application = this.getServletContext();
 		List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
@@ -57,4 +60,62 @@ public class Main extends HttpServlet {
 		}
 
 	}
+	
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		String weight = request.getParameter("weight");
+		String height = request.getParameter("height");
+		
+		
+		// 身長、体重の入力値をプロパティに設定
+		Health health = new Health();
+		health.setHeight(Double.parseDouble(height));
+		health.setWeight(Double.parseDouble(weight));
+		
+		// BMIチェックをして結果を設定
+			BmiCheckLogic bmiCheckLogic = new BmiCheckLogic();
+			bmiCheckLogic.execute(health);
+				
+			// リクエストスコープに保存
+			request.setAttribute("health", health);
+			
+			
+			
+		// 入力値チェック
+		if (text != null && text.length() != 0) {
+			
+			// アプリケーションスコープに保存された投稿を取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+			
+			// セッションスコープに保存されたユーザー情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User)session.getAttribute("loginUser");
+			
+		
+			// 投稿を作成して投稿リストに追加
+		// ここにBMIの算出を入れたい
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			
+			// アプリケーションスコープに投稿リストを保存
+			application.setAttribute("mutterList", mutterList);
+		}
+		
+		
+		// 確認画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/check.jsp");
+		dispatcher.forward(request, response);
+		
+		
+		
 	}
+}
