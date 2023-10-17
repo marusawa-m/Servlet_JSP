45038db6371d5a094f5747e5b86207426faf659a
marusawa.m <marusawa.m@gmail.com>
2023-10-17 14:44:48 +0900

    投稿に関するJavaBeans：修正

--- HealthDiary/src/main/java/model/Mutter.java
+++ HealthDiary/src/main/java/model/Mutter.java
@@ -8,8 +8,7 @@ public class Mutter implements Serializable {
 	
 	private String userName;	// 投稿時の名前
 	private String text;		// メモ欄
-	private double height, weight, bmi;		// BMI計算に使用
-	private String bodyType;	// 実際使用するBMI
+	
 
 	public Mutter (String userName, String text) {
 		this.userName = userName;
@@ -21,32 +20,5 @@ public class Mutter implements Serializable {
 	}
 	public String getText() {
 		return text;
-	}
-	public double getHeight() {
-		return height;
-	}
-	
-	public double getWeight() {
-		return weight;
-	}
-	
-	public double getBmi() {
-		return bmi;
-	}
-	public String getBodyType() {
-		return bodyType;
-	}
-	
-	public void setHeight(double height) {
-		this.height = height;
-	}
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
-	public void setBmi(double bmi) {
-		this.bmi = bmi;
-	}
-	public void setBodyType(String bodyType) {
-		this.bodyType = bodyType;
 	}
 }
