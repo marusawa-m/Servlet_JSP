f9433d3ed84b59f71fd8824b8f014e607ea7a8c8
marusawa.m <marusawa.m@gmail.com>
2023-10-23 09:58:28 +0900

    新規登録：エラーメッセージ修正

--- HealthDiary/src/main/java/model/RegisterUserLogic.java
+++ HealthDiary/src/main/java/model/RegisterUserLogic.java
@@ -6,22 +6,34 @@ public class RegisterUserLogic {
 
     public boolean execute(User user) {
     	
-        // バリデーションを実行
-        if (user.getName() == null || user.getName().trim().isEmpty()) {
-            errorMsg = "名前が入力されていません。";
-            return false; // エラーがある場合は false を返す
-        }
-        
-        if (user.getPass() == null || user.getPass().trim().isEmpty()) {
-            errorMsg = "パスワードが入力されていません。";
-            return false; // エラーがある場合は false を返す
-        }
-        
-        // エラーがない場合は true を返す
-        return true;
-    }
-    
-    public String getErrorMsg() {
-        return errorMsg; // エラーメッセージを取得するメソッド
-    }
+    	  String errorMsg = "";
+
+    	    if (user.getName() == null || user.getName().trim().isEmpty()
+    	            && (user.getPass() == null || user.getPass().trim().isEmpty())) {
+    	    	
+    	        errorMsg = "名前・パスワードが入力されていません.";
+    	        
+    	    } else if (user.getName() == null || user.getName().trim().isEmpty()) {
+    	    
+    	    	errorMsg = "名前が入力されていません.";
+    	    
+    	    } else if (user.getPass() == null || user.getPass().trim().isEmpty()) {
+    	    
+    	    	errorMsg = "パスワードが入力されていません.";
+    	    
+    	    }
+
+    	    if (!errorMsg.isEmpty()) {
+    	        // エラーメッセージが設定されている場合はエラーがあるため、falseを返す
+    	        this.errorMsg = errorMsg;
+    	        return false;
+    	    }
+
+    	    // エラーがない場合はtrueを返す
+    	    return true;
+    	}
+
+    	public String getErrorMsg() {
+    	    return errorMsg; // エラーメッセージを取得するメソッド
+    	}
 }
