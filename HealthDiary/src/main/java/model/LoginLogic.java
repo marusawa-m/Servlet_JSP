eafd7e28a83b3c114c2b4f31b2f762952b4aa436
marusawa.m <marusawa.m@gmail.com>
2023-10-23 16:09:45 +0900

    修正

--- HealthDiary/src/main/java/model/LoginLogic.java
+++ HealthDiary/src/main/java/model/LoginLogic.java
@@ -8,21 +8,24 @@ import java.util.regex.Pattern;
 public class LoginLogic {
 	
 	public boolean execute (User user) {
-
-	// チェックする文字列
-	String str = "java";
+		
+	// ユーザーから提供されたパスワードを取得
+	// (チェックする文字列)
+	String str = user.getPass();
 	
 	// パターンの生成(半角英数4文字)
 	Pattern pattern = Pattern.compile("^[0-9a-zA-Z]{4}|");
-	
+
 	// パターンと一致するか
 	if (pattern.matcher(str).matches()) {
+
+        return true; // パスワードが一致した場合
+
+	} else {
 	
-		// 一致したとき
-		return true;
+		return false;
+	
+    	}
 	}
 	
-	return false;
-	
-	}
 }
