3b1b39849674c3409a8efdd1dccf8db5afb4eec7
marusawa.m <marusawa.m@gmail.com>
2023-10-23 10:10:58 +0900

    ログイン処理モデル：パス個別設定可能

--- HealthDiary/src/main/java/model/LoginLogic.java
+++ HealthDiary/src/main/java/model/LoginLogic.java
@@ -2,19 +2,27 @@ package model;
 
 // ログイン処理を行うモデル
 
+import java.util.regex.Pattern;
+
 
 public class LoginLogic {
 	
 	public boolean execute (User user) {
-		
-		if (user.getPass().equals("0000")) {
-			
-			return true;
-			
-		}
-		
-		return false;
-		
-	}
 
+	// チェックする文字列
+	String str = "java";
+	
+	// パターンの生成(半角英数4文字)
+	Pattern pattern = Pattern.compile("^[0-9a-zA-Z]{4}|");
+	
+	// パターンと一致するか
+	if (pattern.matcher(str).matches()) {
+	
+		// 一致したとき
+		return true;
+	}
+	
+	return false;
+	
+	}
 }
