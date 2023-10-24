e337c06425d5a5552190f19dae4383945e7017b3
marusawa.m <marusawa.m@gmail.com>
2023-10-24 10:03:21 +0900

    投稿のJavaBeans：修正

--- HealthDiary/src/main/java/model/Mutter.java
+++ HealthDiary/src/main/java/model/Mutter.java
@@ -3,6 +3,7 @@ package model;
 // つぶやき投稿の情報に関するJavaBeans
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 
 public class Mutter implements Serializable {
 	
@@ -11,14 +12,17 @@ public class Mutter implements Serializable {
 	public  double bmi;
 	public  String bodyType;
 	private String healthRating;  // 体調評価
+	private SimpleDateFormat date; // 投稿日時
 	
 
-	public Mutter (String userName, String text, double bmi, String bodyType, String healthRating) {
+	public Mutter (String userName, String text, double bmi, String bodyType, String healthRating,
+					SimpleDateFormat date) {
 		this.userName = userName;
 		this.text = text;
 		this.bmi = bmi;
 		this.bodyType = bodyType;
 		this.healthRating = healthRating;
+		this.date = date;
 	}
 	
 	public String getUserName() {
@@ -35,5 +39,8 @@ public class Mutter implements Serializable {
 	}
 	public String getHealthRating() {
 	    return healthRating;
+	}
+	public SimpleDateFormat date() {
+		return date;
 	}
 }
